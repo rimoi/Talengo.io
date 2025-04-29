c718fe03cf002a68e83b37109a5d987456855df6
sidi.lekhalifa <sidi@carizy.com>
2025-04-29 11:03:52 +0200

    update publique

---
+++ public/themes/base/js/custom6.js
@@ -0,0 +1,267 @@
+/*
+Template Name: Miver - LMS & Freelance Services Marketplace for Businesses HTML Template
+Author: Askbootstrap
+Author URI: https://themeforest.net/user/askbootstrap
+Version: 1.0
+*/
+
+$(document).ready(function () {
+	"use strict";
+
+	/* Select2 */
+	$('select').select2();
+
+	/* Tooltip */
+	$('[data-toggle="tooltip"]').tooltip();
+
+	/* index */
+	$('.recent-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+				}
+			}
+
+		]
+	});
+
+	$('.freelance-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+				}
+			}
+
+		]
+	});
+	$('.service-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+				}
+			}
+
+		]
+	});
+	$('.realisation-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+
+		]
+	});
+
+	/* web design */
+	$(function () {
+		$('#aniimated-thumbnials').lightGallery({
+			thumbnail: true,
+		});
+
+		$('.slider-for').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true,
+			fade: true,
+			adaptiveHeight: true,
+			asNavFor: '.slider-nav'
+		});
+
+		$('.recommend').slick({
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			arrows: true,
+			fade: false,
+		});
+
+
+		$('.slider-nav').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			asNavFor: '.slider-for',
+			dots: false,
+			arrows: true,
+			focusOnSelect: true,
+			variableWidth: true,
+			responsive: [{
+					breakpoint: 1099,
+					settings: {
+						slidesToShow: 4,
+					}
+				},
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+					}
+				}
+
+			]
+		});
+	});
+	/* profile */
+
+	/* wireframe */
+	$('#aniimated-thumbnials').lightGallery({
+		thumbnail: true,
+	});
+
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		adaptiveHeight: true,
+		asNavFor: '.slider-nav'
+
+	});
+
+	$('.recommend').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 3,
+				}
+			}
+
+		]
+	});
+
+	$(".view").not('.slick-initialized').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: false,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+
+		]
+	});
+
+	$('.slider-nav').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: false,
+		arrows: true,
+		focusOnSelect: true,
+		variableWidth: true,
+		responsive: [{
+				breakpoint: 1099,
+				settings: {
+					slidesToShow: 4,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+
+		]
+	});
+
+
+});
